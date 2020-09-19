95739b597584de5af8dd7fc3e1b1ade0b465d323
henriquedeof <henriquedeof@gmail.com>
2020-09-19 18:31:37 +1000

    First commit.

---
+++ filmaria/src/components/Header/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./header.css";
+import {Link} from "react-router-dom";
+
+class Header extends React.Component{
+
+    render() {
+        return(
+            <div className="header">
+                <Link to="/">Filmaria</Link>
+            </div>
+        );
+    }
+
+}
+
+export default Header;
---
+++ filmaria/src/pages/Home/index.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import {Link} from "react-router-dom";
+import "./home.css";
+
+class Home extends React.Component{
+
+    constructor() {
+        super();
+        this.state = {
+            filmes: []
+        }
+    }
+
+    componentDidMount() {
+        this.loadFilmes();
+    }
+
+    loadFilmes = () =>{
+        //https://sujeitoprogramador.com/r-api/?api=filmes
+        let url = 'https://sujeitoprogramador.com/r-api/?api=filmes';
+        fetch(url)
+        .then(r => r.json())
+        .then(json =>{
+            this.setState({filmes: json});
+            console.log(json);
+        });
+    }
+
+    render() {
+        return(
+            <div className="container">
+
+                <div className="lista-filmes">
+                    {this.state.filmes.map(filme =>{
+                        return(
+                            <article key={filme.id} className="filme">
+                                <strong>{filme.nome}</strong>
+                                <img src={filme.foto} alt="capa" />
+                                <Link to={`/filme/${filme.id}`}>Acessar</Link>
+                            </article>
+                        )
+                    })}
+                </div>
+
+            </div>
+        );
+    }
+
+}
+
+export default Home;
